5f611cec026e4659da9a81b64a040c4f5d028852
Rafael Corzo <rcorzogutierrez@gmail.com>
2023-01-16 20:46:51 -0500

    first-stage

--- src/app/components/list-clientes/list-clientes.component.ts
+++ src/app/components/list-clientes/list-clientes.component.ts
@@ -1,19 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { map, Observable } from 'rxjs';
+import { ClientesService } from 'src/app/services/clientes.service';
 
 @Component({
   selector: 'app-list-clientes',
   templateUrl: './list-clientes.component.html',
   styleUrls: ['./list-clientes.component.css']
 })
-export class ListClientesComponent{
-  items:Observable<any[]>;
-  
-  constructor(firestore:AngularFirestore){
-    this.items = firestore.collection('items').valueChanges();
-   
+export class ListClientesComponent {
+  clientes: any[] = [];
+  clientprojects: any[] = [];
+  projects: any[] = [];
+  clientId = '0TYkv0jIVRMyPagqI3d3';
+
+
+  constructor(private _clienteService: ClientesService, private toastr: ToastrService) {
+
   }
-  
+  ngOnInit() {
+    this.getClientes();
+  }
+
+  getClientes() {
+    this._clienteService.getClientes().subscribe(data => {
+      this.clientes = [];
+      data.forEach((element: any) => {
+
+        this.clientes.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data()
+        })
+      });
+      //console.log(this.clientes);
+    });
+  }
+
+  eliminarCliente(id:string){
+    this._clienteService.eliminarCliente(id).then(()=>{
+      console.log('eliminado')
+      this.toastr.error('El cliente fue eliminado con exito', 'Cliente Eliminado', { positionClass: 'toast-top-right' });
+    })
+  }
+
 
 }
+
+ 
+
+
+
